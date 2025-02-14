56518e12d94496c2de1ab5b65830e77acedf72f1
ferbarcante <barcantef@gmail.com>
2025-02-14 08:19:51 -0300

    criação de entidades

---
+++ src/main/java/com/example/chat/Entities/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.example.chat.Entities;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
--- src/main/java/com/example/chat/controller/ChatController.java
+++ src/main/java/com/example/chat/controller/ChatController.java
@@ -1,4 +1,18 @@
 package com.example.chat.controller;
 
+import com.example.chat.Entities.ChatMessage;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
 public class ChatController {
+
+    @MessageMapping
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage)
+    {
+
+    }
 }
