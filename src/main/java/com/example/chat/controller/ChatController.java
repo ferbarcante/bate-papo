10829b898ef9fc717bf4941dffd9a9719126594a
ferbarcante <barcantef@gmail.com>
2025-02-14 08:22:21 -0300

    metodo para enviar mensagem

--- src/main/java/com/example/chat/controller/ChatController.java
+++ src/main/java/com/example/chat/controller/ChatController.java
@@ -4,15 +4,17 @@ import com.example.chat.Entities.ChatMessage;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 
 @Controller
 public class ChatController {
 
-    @MessageMapping
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage)
     {
-
+        return chatMessage;
     }
 }
